adbe78b9cbb0af61eedd2385204f5d701ca7bbed
GeorgieSantiago <milestoneartisan@gmail.com>
2021-01-24 23:31:48 -0500

    high level configuration

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,38 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'transform-inline-environment-variables',
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: [
-          '.ios.ts',
-          '.android.ts',
-          '.ts',
-          '.ios.tsx',
-          '.android.tsx',
-          '.tsx',
-          '.jsx',
-          '.js',
-          '.json',
-        ],
-        alias: {
-          components: './src/components',
-          interfaces: './src/interfaces',
-          store: './src/store',
-          styles: './src/styles',
-          template: './src/template',
-          screens: './src/screens',
-          router: './src/router',
-          network: './src/network',
-          locales: './src/locales',
-          useAction: './src/hooks/useAction.tsx',
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      'transform-inline-environment-variables',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            components: './src/components',
+            interfaces: './src/interfaces',
+            store: './src/store',
+            styles: './src/styles',
+            template: './src/template',
+            screens: './src/screens',
+            router: './src/router',
+            network: './src/network',
+            locales: './src/locales',
+            useAction: './src/hooks/useAction.tsx',
+          },
         },
-      },
+      ],
     ],
-  ],
+  };
 };
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: path.resolve(__dirname, './public/index.html'),
+  filename: 'index.html',
+  inject: 'body',
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+})
+
+module.exports = {
+  entry: path.join(__dirname, 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/build'),
+  },
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web',
+      '@storybook/react-native': '@storybook/react',
+      'styled-components/native': 'styled-components',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!()\/).*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [HTMLWebpackPluginConfig],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './storybook',
+    hot: true,
+  },
+}
